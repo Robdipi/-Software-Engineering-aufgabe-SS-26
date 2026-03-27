a0c76894de2ac1809ed859fe72b0341a500ff08e
robin <rd.studios.germany@gmail.com>
2026-03-27 12:25:13 +0100

    start commit

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,7 +2,7 @@ import scala.io.StdIn.readLine
 
 object main {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    println("Tmp, World!")
     val greeting = "hello " + signup(args)
     print(greeting)
   }
