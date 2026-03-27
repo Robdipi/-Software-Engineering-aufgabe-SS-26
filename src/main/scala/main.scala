9832cf58034409f08682305110e7b16a050c4e44
robin <rd.studios.germany@gmail.com>
2026-03-27 12:03:43 +0100

    start commit

---
+++ src/main/scala/main.scala
@@ -0,0 +1,15 @@
+import scala.io.StdIn.readLine
+
+object main {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+    val greeting = "hello " + signup(args)
+    print(greeting)
+  }
+  def signup(strings: Array[String]):String = {
+    if(strings.length > 0)
+      strings.head
+    else
+      readLine("Please enter Name")
+  }
+}
